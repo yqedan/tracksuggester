cb44404bcc8030bba2b0438ee2fb7a920751dc1a
Yusuf Qedan <yusuf9191@gmail.com>
2016-08-12 13:53:00 -0700

    remove image directory, add links to all images, add empty form value error checking, styling

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,16 @@ var blobFun = function(yesOrNo,trackSuggested){
     return trackSuggested;
 }
 var displayImage = function(trackSuggested){
-  return "<img id='result'src='./img/java.png'class='img-responsive'>"
+  if(trackSuggested === "Java")
+    return "<img id='result'src='http://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/5759b4634c2f85443c0a18b4/1465496687081/?format=300w'class='img-responsive'>";
+  else if(trackSuggested === "Design")
+    return "<img id='result'src='http://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/5759b22cf8baf31aa1970715/1465496112479/Design-100.png?format=300w'class='img-responsive'>";
+  else if(trackSuggested === "C#")
+    return "<img id='result'src='http://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/5759d001ab48de9430869023/1465503752895/?format=300w'class='img-responsive'>";
+  else if(trackSuggested === "Ruby")
+    return "<img id='result'src='http://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/5759b14b22482e7d6ed0fef8/1465495889919/?format=300w'class='img-responsive'>";
+  else // Track is PHP
+    return "<img id='result'src='http://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/5759b71c62cd94159d74a83c/1465497380038/?format=300w'class='img-responsive'>";
 }
 $(document).ready(function() {
   $("form").submit(function(event){
@@ -50,6 +59,14 @@ $(document).ready(function() {
     var operatingSystem = $("input:radio[name=os]:checked").val();
     var gemstone = $("input:radio[name=gem]:checked").val();
     var blob = $("input:radio[name=blob]:checked").val();
+    if(!name){
+      alert("please enter your name");
+      return;
+    }
+    if((!develop)||(!size)||(!operatingSystem)||(!gemstone)||(!blob)){
+      alert("please answer all of the questions");
+      return;
+    }
     $('#result').remove();
     track = developFun(develop);
     track = companyFun(size,track);
