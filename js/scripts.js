0d9eef6f98c4de0354f20f1264d74091de7e100b
Yusuf Qedan <yusuf9191@gmail.com>
2016-08-12 10:08:47 -0700

    add some more business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var developFun = function(frontOrBack){
   if(frontOrBack === "front")
     return "Design";
-  else //frontOrBack === "back"
+  else //frontOrBack  is back
     return "";
 }
 var companyFun = function(bigOrSmall,trackSuggested){
@@ -10,20 +10,47 @@ var companyFun = function(bigOrSmall,trackSuggested){
       return "Ruby";
     else if(bigOrSmall === "medium")
       return "Java";
-    else //bigOrSmall === "large"
+    else //bigOrSmall is large
       return "C#";
   }
   else
     return trackSuggested;
 }
-
+var operatingSystemFun = function(macPcLinux,trackSuggested){
+  if(trackSuggested === "C#"){
+    if(macPcLinux === "pc")
+      return trackSuggested;
+    else //liunx or mac
+      return "Java";
+  }
+  else
+      return trackSuggested;
+}
+var gemstoneFun = function(yesOrNo,trackSuggested){
+  if(yesOrNo === "yes")
+    return "Ruby"
+  else
+    return trackSuggested;
+}
+var blobFun = function(yesOrNo,trackSuggested){
+  if(yesOrNo === "yes")
+    return "PHP"
+  else
+    return trackSuggested;
+}
 $(document).ready(function() {
   $("form").submit(function(event){
     var track = "";
     var develop = $("input:radio[name=develop]:checked").val();
     var size = $("input:radio[name=size]:checked").val();
+    var operatingSystem = $("input:radio[name=os]:checked").val();
+    var gemstone = $("input:radio[name=gem]:checked").val();
+    var blob = $("input:radio[name=blob]:checked").val();
     track = developFun(develop);
     track = companyFun(size,track);
+    track = operatingSystemFun(operatingSystem,track);
+    track = gemstoneFun(gemstone,track);
+    track = blobFun(blob,track);
     alert(track);
     $("#hiddenMessage").show();
     event.preventDefault();
